6c11bb2667417fbf1097835ac894e560b0fb8d85
aoi0318 <hamaoka3018@gmail.com>
2024-02-25 01:57:04 +0900

    リファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,72 @@ import 'package:nfc_manager/nfc_manager.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('テスト')),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(100),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FutureBuilder<bool>(
-                  future: NfcManager.instance.isAvailable(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const CircularProgressIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      return Text('NFCstate: ${snapshot.data}');
-                    }
-                  },
-                ),
-                const SizedBox(height: 20), // 余白を追加
-                RadioExample(),
-              ],
-            ),
-          ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('テスト')),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(100),
+          child: MyContent(),
         ),
       ),
     );
   }
 }
 
-enum StateList { clockIn, clockOut, startBreak, endBreak }
+class MyContent extends StatelessWidget {
+  const MyContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        NFCStatusWidget(),
+        SizedBox(height: 20), // 余白を追加
+        RadioExample(),
+      ],
+    );
+  }
+}
+
+class NFCStatusWidget extends StatelessWidget {
+  const NFCStatusWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: NfcManager.instance.isAvailable(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Text('NFCstate: ${snapshot.data}');
+        }
+      },
+    );
+  }
+}
 
 class RadioExample extends StatefulWidget {
-  const RadioExample({Key? key}) : super(key: key);
+  const RadioExample({super.key});
 
   @override
   State<RadioExample> createState() => _RadioExampleState();
@@ -57,48 +82,31 @@ class _RadioExampleState extends State<RadioExample> {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        RadioListTile(
-          title: const Text('出勤'),
-          value: StateList.clockIn,
+      children: StateList.values.map((state) {
+        return RadioListTile<StateList>(
+          title: Text(describeEnum(state)),
+          value: state,
           groupValue: _state,
-          onChanged: (value) {
-            setState(() {
-              _state = value!;
-            });
-          },
-        ),
-        RadioListTile(
-          title: const Text('退勤'),
-          value: StateList.clockOut,
-          groupValue: _state,
-          onChanged: (value) {
-            setState(() {
-              _state = value!;
-            });
-          },
-        ),
-        RadioListTile(
-          title: const Text('休憩開始'),
-          value: StateList.startBreak,
-          groupValue: _state,
-          onChanged: (value) {
-            setState(() {
-              _state = value!;
-            });
-          },
-        ),
-        RadioListTile(
-          title: const Text('休憩終了'),
-          value: StateList.endBreak,
-          groupValue: _state,
-          onChanged: (value) {
-            setState(() {
-              _state = value!;
-            });
-          },
-        ),
-      ],
+          onChanged: (value) => setState(() => _state = value!),
+        );
+      }).toList(),
     );
   }
+
+  String describeEnum(StateList state) {
+    switch (state) {
+      case StateList.clockIn:
+        return '出勤';
+      case StateList.clockOut:
+        return '退勤';
+      case StateList.startBreak:
+        return '休憩開始';
+      case StateList.endBreak:
+        return '休憩終了';
+      default:
+        return '';
+    }
+  }
 }
+
+enum StateList { clockIn, clockOut, startBreak, endBreak }
